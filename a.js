2babb2fed4d3ff4751ccd7716031001a90bc1e85
Mradxz <mradxz@gmail.com>
2013-04-09 17:01:04 +0800

    Create a.js

---
+++ a.js
@@ -0,0 +1,21 @@
+// JavaScript Document
+document.writeln("<script language=\"javascript\">");
+document.writeln("");
+document.writeln("var LR_next_invite_seconds = 80;");
+document.writeln("var LrinviteTimeout = 0;");
+document.writeln("var LiveAutoInvite0=\'云南省皮肤科诊疗基地 （医保定点单位）\';");
+document.writeln("var LiveAutoInvite1=\'来自首页的对话\';");
+document.writeln("var LiveAutoInvite2 = \'<style type=\"text/css\">\';");
+document.writeln("LiveAutoInvite2 += \'<!--\';");
+document.writeln("LiveAutoInvite2 += \'.iframe {margin:0px 0px 0px 0px;;}\';");
+document.writeln("LiveAutoInvite2 += \'-->\';");
+document.writeln("LiveAutoInvite2 += \'</style>\';");
+document.writeln("LiveAutoInvite2 +=\'<div style=\" width:420px; height:35px; background-image:url(http://www.pifukyy.com/img/swt1.gif); padding-top:165px; margin-top:-20px; margin-left:-10px; margin-right:-10px;\"><a href=\"http://dlt.zoosnet.net/LR/Chatpre.aspx?id=DLT74847936&p=tuiguang_hj\" target=\"_blank\"><img src=\"http://www.pifukyy.com/img/swt2.gif\" width=\"113\" height=\"33\" style=\"border:none; margin-left:160px; margin-right:10px; float:left;\" /></a><a href=\"http://dlt.zoosnet.net/LR/Chatpre.aspx?id=DLT74847936&p=tuiguang_hj\" target=\"_blank\"><img src=\"http://www.pifukyy.com/img/swt3.gif\" width=\"115\" height=\"35\" style=\"border:none; float:left;\" /></a></div>\';");
+document.writeln("LiveAutoInvite2 += \'<iframe class=\"iframe\" scrolling=no height=1 frameborder=0 width=400 align=center src=\"http://www.pifukyy.com/img/index.html\" marginwidth=0 marginheight=0>\';");
+document.writeln("</script>");
+document.writeln("<script language=\"javascript\" src=\"http://DLT.zoosnet.net/JS/LsJS.aspx?siteid=DLT74847936&float=0&lng=cn\"></script> ");
+document.writeln("");
+
+document.writeln("<script  src=\"http://www.pifukyy.com/js/tgbottom.js\"></script>");
+document.writeln("<SCRIPT src=\"http://www.pifukyy.com/js/jquery-1.4.2.min.js\" type=\"text/javascript\"></SCRIPT>");
+document.writeln("<script  src=\"http://www.pifukyy.com/js/tgduo1.js\" ></script>");
